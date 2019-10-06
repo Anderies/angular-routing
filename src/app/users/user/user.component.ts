d3a3487eed73d6f55bc7a6cc03a420aeae508e26
Andreas <ndreznotanto@gmail.com>
2019-10-06 17:03:06 +0700

    Fetching Route Parameters Reactively

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -13,10 +13,18 @@ export class UserComponent implements OnInit {
 
   ngOnInit() {
     this.user = {
-      // we retrieve id from users/:id app.module
+      // we retrieve id from users/:id user/:name app.module
       id: this.currentRoute.snapshot.params['id'],
       name: this.currentRoute.snapshot.params['name']
     }
+    // this will be executed when params changed
+    this.currentRoute.params.subscribe(
+      // it will be fired whenever new data sent through observable(whenever parameters url change)
+      (params: Params) => {
+        this.user.id = params['id'];
+        this.user.name = params['name'];
+      }
+    );
   }
 
 }
